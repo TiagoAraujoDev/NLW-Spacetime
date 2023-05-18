a4f9472ffe7f5f1d31caf76335d5b8df27311499
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2023-05-18 11:27:31 +0100

    Refactor and add href to github authorize page

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export const Copyright = () => {
+  return (
+    <p className="text-sm leading-relaxed text-gray-200">
+      Feito com 💜 no NLW da{' '}
+      <a
+        target="_blank"
+        href="https://rocketseat.com.br"
+        rel="noreferrer"
+        className="underline hover:text-gray-100"
+      >
+        Rocketseat
+      </a>
+    </p>
+  )
+}
