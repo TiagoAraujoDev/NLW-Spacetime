c9e49f836095cbb1feb37f95fd8749ca1591a49e
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2023-05-18 12:50:00 +0100

    Add auth route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,10 @@
+import "dotenv/config";
 import fastify from "fastify";
 import cors from "@fastify/cors";
 
 import memoriesRoutes from "./routes/memories";
+import authRoutes from "./routes/auth";
+import jwt from "@fastify/jwt";
 
 const PORT = 3333;
 
@@ -10,6 +13,11 @@ const app = fastify();
 app.register(cors, {
   origin: true,
 });
+app.register(jwt, {
+  secret: "tma-spacetime",
+});
+
+app.register(authRoutes);
 app.register(memoriesRoutes);
 
 app
