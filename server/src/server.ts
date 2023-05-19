40344b05aa3e8f357741951d411a49330029e46a
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2023-05-19 14:50:09 +0100

    feat: media upload

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,25 @@
 import "dotenv/config";
 import fastify from "fastify";
 import cors from "@fastify/cors";
+import jwt from "@fastify/jwt";
+import multipart from "@fastify/multipart";
+import fastifyStatic from "@fastify/static";
 
 import memoriesRoutes from "./routes/memories";
 import authRoutes from "./routes/auth";
-import jwt from "@fastify/jwt";
+import uploadRoutes from "./routes/upload";
+import { resolve } from "path";
 
 const PORT = 3333;
 
 const app = fastify();
 
+// Modules
+app.register(fastifyStatic, {
+  root: resolve(__dirname, "..", "uploads"),
+  prefix: "/uploads",
+});
+app.register(multipart);
 app.register(cors, {
   origin: true,
 });
@@ -17,7 +27,9 @@ app.register(jwt, {
   secret: "tma-spacetime",
 });
 
+// Routes
 app.register(authRoutes);
+app.register(uploadRoutes);
 app.register(memoriesRoutes);
 
 app
